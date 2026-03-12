78098b348a657c509063f3237e189e6e1d95b57c
Shahid Raza <raza59261@gmail.com>
2026-03-12 22:52:54 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  const [display,setDisplay] = useState(false);
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <h2>Button Action</h2>
+      <button id="click" onClick={() => setDisplay(!display)}>
+        {display ? "Hide" : "Show"} Content
+      </button>
+      {display && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
